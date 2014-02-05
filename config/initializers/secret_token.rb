fbf68d80e9222f1fd341ff9966b7317a33ed3dee
jeffersonsc <jeffersondasilva14@gmail.com>
2014-02-05 11:54:29 -0200

    inicio do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ControleLivros::Application.config.secret_key_base = '47827b98dbd5905ae9b0afc099db6bb75b4fdcb4aea9988e8185cb3a36e618e3343d7c90fb34064140a9fa8b44f6f6f55c0064496aef315b0e0bc376d285076a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ControleLivros::Application.config.session_store :cookie_store, key: '_controle_livros_session'
